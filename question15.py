281ce00a5638fb90e97539dc73c2c9c8f58e5756
vaibhavi929 <vaibhavigupta10a@gmail.com>
2026-03-13 00:35:51 +0530

    Added solution for Question 15 - Reverse an array

---
+++ question15.py
@@ -0,0 +1,12 @@
+# Question 15
+# Problem: Reverse an array
+# Name: Vaibhavi Gupta
+# Course: BTech DS + AI
+
+def reverse_array(arr):
+    return arr[::-1]
+
+arr = [5,4,3,2,1]
+
+result = reverse_array(arr)
+print(result)

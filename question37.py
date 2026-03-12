916a57ff9b9df1dc7d2cf45f8daf5add6b82bf00
vaibhavi929 <vaibhavigupta10a@gmail.com>
2026-03-13 01:28:05 +0530

    Added solution for Question 37 - Convert string to uppercase

---
+++ question37.py
@@ -0,0 +1,11 @@
+# Question 37
+# Problem: Convert string to uppercase
+# Name: Vaibhavi Gupta
+# Course: BTech DS + AI
+
+def to_uppercase(s):
+    return s.upper()
+
+s = "data science"
+
+print(to_uppercase(s))

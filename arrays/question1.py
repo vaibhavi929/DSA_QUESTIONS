2fc65cc079ce09bf2f3c5f9c6ff4adde8264081b
vaibhavi929 <vaibhavigupta10a@gmail.com>
2026-03-13 00:01:04 +0530

    Added Array Question 1 solution

---
+++ arrays/question1.py
@@ -0,0 +1,17 @@
+# Function to check if a number is prime
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+# Input
+num = int(input("Enter a number: "))
+
+# Output
+if is_prime(num):
+    print("True")
+else:
+    print("False")

70314eefab642457fee9b0f9a6036278c911e60e
vaibhavi929 <vaibhavigupta10a@gmail.com>
2026-03-13 01:02:55 +0530

    Added solution for Question 30 - Reverse a string

---
+++ question30.py
@@ -0,0 +1,12 @@
+# Question 30
+# Problem: Reverse a string
+# Name: Vaibhavi Gupta
+# Course: BTech DS + AI
+
+def reverse_string(s):
+    return s[::-1]
+
+
+s = "Capgemini"
+
+print(reverse_string(s))

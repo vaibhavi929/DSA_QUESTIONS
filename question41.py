ce83d930a03ffdc2ef16413bff119db0a8a111d2
vaibhavi929 <vaibhavigupta10a@gmail.com>
2026-03-13 22:55:35 +0530

    Added solution for Question 41 - Queue implementation using list

---
+++ question41.py
@@ -0,0 +1,18 @@
+# Question 41
+# Problem: Implement Queue using list
+# Name: Vaibhavi Gupta
+# Course: BTech DS + AI
+
+queue = []
+
+# Enqueue
+queue.append(10)
+queue.append(20)
+queue.append(30)
+
+print("Queue after enqueue:", queue)
+
+# Dequeue
+queue.pop(0)
+
+print("Queue after dequeue:", queue)

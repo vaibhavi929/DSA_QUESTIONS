f0f789e9437ec1ea522db677a78a900b08151332
vaibhavi929 <vaibhavigupta10a@gmail.com>
2026-03-13 22:54:48 +0530

    Added solution for Question 40 - Check if stack is empty

---
+++ question40.py
@@ -0,0 +1,14 @@
+# Question 40
+# Problem: Check if stack is empty
+# Name: Vaibhavi Gupta
+# Course: BTech DS + AI
+
+stack = []
+
+def is_empty(stack):
+    if len(stack) == 0:
+        return True
+    else:
+        return False
+
+print(is_empty(stack))

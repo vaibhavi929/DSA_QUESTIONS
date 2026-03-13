b73718eecefcf2d08e144b12e4f367d1e3c1b6b9
vaibhavi929 <vaibhavigupta10a@gmail.com>
2026-03-13 22:54:17 +0530

    Added solution for Question 39 - Stack implementation using list

---
+++ question39.py
@@ -0,0 +1,18 @@
+# Question 39
+# Problem: Implement Stack using Python list
+# Name: Vaibhavi Gupta
+# Course: BTech DS + AI
+
+stack = []
+
+# Push operation
+stack.append(10)
+stack.append(20)
+stack.append(30)
+
+print("Stack after push:", stack)
+
+# Pop operation
+stack.pop()
+
+print("Stack after pop:", stack)

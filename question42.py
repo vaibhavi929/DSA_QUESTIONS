68e9e8ef901609ec60ce48796e3554660d4d9b14
vaibhavi929 <vaibhavigupta10a@gmail.com>
2026-03-13 22:56:04 +0530

    Added solution for Question 42 - Reverse a queue

---
+++ question42.py
@@ -0,0 +1,12 @@
+# Question 42
+# Problem: Reverse a queue
+# Name: Vaibhavi Gupta
+# Course: BTech DS + AI
+
+from collections import deque
+
+queue = deque([1,2,3,4,5])
+
+queue.reverse()
+
+print(list(queue))
